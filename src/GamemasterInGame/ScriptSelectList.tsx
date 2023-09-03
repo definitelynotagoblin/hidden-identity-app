3f7db21062d603b7115aa656cf0bdaf86c7e628a
vermentor <nemeth.alex@protonmail.com>
2023-09-02 18:06:32 -0700

    Add custom scipt import to ScriptSelectList

--- src/GamemasterInGame/ScriptSelectList.tsx
+++ src/GamemasterInGame/ScriptSelectList.tsx
@@ -1,23 +1,44 @@
-import { Box, Grid, Flex, Button } from "@radix-ui/themes";
+import {
+  Box,
+  Grid,
+  Flex,
+  Button,
+  Heading,
+  TextArea,
+  Callout,
+} from "@radix-ui/themes";
 import GameData from "../assets/game_data/scripts.json";
 import React from "react";
 
 interface ScriptSelectListProps {
-  handleSubmit: (script: string) => void;
+  handleSubmit: (script: string, customScript: unknown) => void;
+  enableCustom?: boolean;
 }
 
-function ScriptSelectList({ handleSubmit }: ScriptSelectListProps) {
+function ScriptSelectList({
+  handleSubmit,
+  enableCustom = true,
+}: ScriptSelectListProps) {
   const [selectedScript, setSelectedScript] = React.useState("");
+  const [customScript, setCustomScript] = React.useState("");
+  const [errorMsg, setErrorMsg] = React.useState("");
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        handleSubmit(selectedScript);
+        try {
+          const parsedCustomScript = JSON.parse(customScript);
+          // TODO: verify parsedCustomScript is correct type
+          handleSubmit(selectedScript, parsedCustomScript);
+          setErrorMsg("");
+        } catch (e) {
+          setErrorMsg("Custom script is not valid JSON.");
+        }
       }}
     >
-      <Flex direction={"column"} align={"center"}>
-        <Grid columns="1" align={"center"}>
+      <Flex gap="4" direction={"column"} align={"center"}>
+        <Grid gap="3" columns="1" align={"center"}>
           {GameData.scripts.map(({ name, imageSrc }) => (
             <Box
               key={name}
@@ -32,7 +53,42 @@ function ScriptSelectList({ handleSubmit }: ScriptSelectListProps) {
               <img width="300px" src={imageSrc} />
             </Box>
           ))}
+          {enableCustom && (
+            <Box
+              key="custom-script"
+              style={{
+                textAlign: "center",
+                border:
+                  "custom-script" === selectedScript
+                    ? "4px solid darkred"
+                    : "none",
+                borderRadius: "15px",
+              }}
+              onClick={() => {
+                setSelectedScript("custom-script");
+              }}
+            >
+              <label htmlFor="custom-input">
+                <Heading as="h1">CUSTOM</Heading>
+              </label>
+              {"custom-script" === selectedScript && (
+                <TextArea
+                  id="custom-input"
+                  placeholder="[ { 'id': 'Washerwoman' }, ... ]"
+                  value={customScript}
+                  onChange={(event) => {
+                    setCustomScript(event.currentTarget.value);
+                  }}
+                ></TextArea>
+              )}
+            </Box>
+          )}
         </Grid>
+        {errorMsg && (
+          <Callout.Root>
+            <Callout.Text>{errorMsg}</Callout.Text>
+          </Callout.Root>
+        )}
         <Button type="submit" disabled={!selectedScript}>
           Select Script
         </Button>
